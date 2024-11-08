c90b10f7e862e44f3c099c6ebbb21acbff7817e8
UdayNain <106189436+UdayNain@users.noreply.github.com>
2024-11-07 21:14:39 -0800

    Update week1_exercise1.cpp

--- week1_exercise1.cpp
+++ week1_exercise1.cpp
@@ -4,11 +4,14 @@
 //IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
+DigitalOut output(LED1);
+DigitalIn button(p5);
+
 int main() {
     while (1) {
         printf("Week 1 Exercise 1");
-
+         output=button;
         // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
-        wait_ms(500); 
+        wait_ms(100); 
     }
 }
